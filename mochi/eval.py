aa513ae6984fdd5700576cd7f74ef8982937dbde
Chunpai <chunpaiwang@gmail.com>
2021-12-15 01:50:31 -0500

    add MasteryGrids data

--- mochi/eval.py
+++ mochi/eval.py
@@ -2,6 +2,8 @@ __author__ = "Chunpai W."
 __email__ = "cwang25@albany.edu"
 
 import pickle
+import random
+
 import numpy as np
 from numpy.random import RandomState
 
@@ -9,67 +11,47 @@ from mochi.utils import *
 from scipy.stats import spearmanr
 
 
-def eval_policy(policy_name, top_k, user_list, pretest_and_posttest_dict, next_item_distribution_dict):
+def eval_policy(policy_name, top_k, users, items, user_records, pretest_posttest_dict,
+                next_item_distr_dict):
     traj_dch_list = []
     traj_ipw_dch_list = []
     traj_score_growth_list = []
     traj_posttest_score_list = []
     # sample a test user
-    for traj, user_name in enumerate(user_list):
-        # reset the test student models' initial belief when new trajectory is sampled as the
-        # training user's initial belief
-        pretest = pretest_list[traj]
-        user_q_list = users_question_list[traj]
-        user_a_list = users_answer_list[traj]
-        pretest, posttest = pretest_and_posttest_dict[user_name]
+    for traj, user_name in enumerate(users):
+        user_q_list = user_records[user_name]
+        pretest, posttest = pretest_posttest_dict[user_name]
         score_growth = posttest - pretest
         traj_score_growth_list.append(score_growth)
-
         traj_posttest_score_list.append(posttest)
-        prPurple("\ntraj:{}/{}, user:{}, pretest:{}, posttest:{}".format(
-            traj, len(users_name_list), user_name, pretest, posttest))
 
-        q_list = []
-        a_list = []
         dch = []
         ipw_dch = []
-        for t in range(traj_lengths[traj] if type(traj_lengths) == list else traj_lengths):
+        for t in range(len(user_records[user_name])):
             # generate an action that follows policy (for BKT-MP, it is based on curr_bel)
             # once an action is generated, it is removed from the action space (problem_list)
 
-            if policy == "baseline":
-                true_question = user_q_list[t]
-                idx = problem_seq.index(true_question)
-                if idx + 1 != len(problem_seq):
-                    question = problem_seq[idx + 1]
-                    candidates = problem_seq[idx + 1: idx + top_k + 1]
-                else:
-                    question = problem_seq[-1]
-                    candidates = problem_seq[-top_k:][::-1]
+            if policy_name == "random":
+                candidates = random.sample(items, top_k)
             else:
-                candidates = dkt_agent.plan(policy)
-                print(candidates)
-                question = candidates[0]
+                raise NotImplementedError
 
             if t + 1 < len(user_q_list):
                 current_q = user_q_list[t]
-                current_score = user_a_list[t]
                 true_next_question = user_q_list[t + 1]
                 if true_next_question in candidates:
                     candidates = list(candidates)
                     prBlue("HIT: student {}".format(user_name))
-                    print("current question and score: ({}, {:.3f})".format(
-                        current_q, current_score))
                     print("candidates {}".format(candidates))
                     print("true next question: {}".format(true_next_question))
                     print("true next question distribution (next-q: count): {}".format(
-                        next_item_distribution_dict[current_q]))
+                        next_item_distr_dict[current_q]))
                     ndch = 1.0 / np.log2(candidates.index(true_next_question) + 1 + 1)
                     total_count = 0.
-                    for question in next_item_distribution_dict[current_q].keys():
-                        total_count += next_item_distribution_dict[current_q][question]
+                    for question in next_item_distr_dict[current_q].keys():
+                        total_count += next_item_distr_dict[current_q][question]
 
-                    propensity_score = next_item_distribution_dict[current_q][
+                    propensity_score = next_item_distr_dict[current_q][
                                            true_next_question] / total_count
                     propensity_ndch = ndch / propensity_score
                     prGreen("ndch score {} , propensity score: {}, propensity ndch score {}".format(
@@ -79,12 +61,10 @@ def eval_policy(policy_name, top_k, user_list, pretest_and_posttest_dict, next_i
                     dch.append(ndch)
                 else:
                     prRed("MISS: student {}".format(user_name))
-                    print(
-                        "current question and score: ({}, {:.3f})".format(current_q, current_score))
                     print("candidates {}".format(candidates))
                     print("true next question: {}".format(true_next_question))
                     print("true next question distribution (next-q: count): {}".format(
-                        next_item_distribution_dict[current_q]))
+                        next_item_distr_dict[current_q]))
                     print('-' * 20)
                     dch.append(0.)
                     ipw_dch.append(0.)
@@ -134,17 +114,27 @@ def eval_policy(policy_name, top_k, user_list, pretest_and_posttest_dict, next_i
            correlation_ipw_dch_post_score, correlation_ipw_dch_score_growth
 
 
-
+def get_data(data_str):
+    data = pickle.load(open("../data/{}/data.pkl".format(data_str), "rb"))
+    user_list = data["users"]
+    item_list = data['items']
+    user_records_dict = data["records"]
+    pretest_posttest_dict = data["pretest_posttest_dict"]
+    next_item_distr_dict = data["next_items_dict"]
+    return user_list, item_list, user_records_dict, pretest_posttest_dict, next_item_distr_dict
 
 
 if __name__ == '__main__':
-    fold = 1
-    top_k = 1
-
+    data_str = "MasteryGrids"
+    top_k = 3
     policy = "random"
+    sample_size = 1
+
+    users, items, user_records, pretest_posttest_dict, next_item_distr_dict = get_data(data_str)
 
     if policy != "random":
-        eval_policy(policy, top_k, pretest_and_posttest_dict, next_item_distribution_dict)
+        eval_policy(policy, top_k, users, items, user_records, pretest_posttest_dict,
+                    next_item_distr_dict)
     else:
         traj_lengths_list = []
         avg_dch_list_list = []
@@ -169,51 +159,32 @@ if __name__ == '__main__':
         correlation_ipw_dch_simulated_reward_list = []
         pvalue_ipw_dch_simulated_reward_list = []
 
-        for i in range(100):
-            outputs = eval_policy(policy, top_k, pretest_and_posttest_dict, next_item_distribution_dict)
-            traj_lengths_list.append(outputs[0])
-            avg_dch_list_list.append(outputs[1])
-            avg_ipw_dch_list_list.append(outputs[2])
-            traj_posttest_score_list_list.append(outputs[3])
-            traj_score_growth_list_list.append(outputs[4])
-            final_rewards_list.append(outputs[5])
-            correlation_dch_post_score_list.append(outputs[6].correlation)
-            pvalue_dch_post_score_list.append(outputs[6].pvalue)
-            correlation_dch_traj_length_list.append(outputs[7].correlation)
-            pvalue_dch_traj_length_list.append(outputs[7].pvalue)
-            correlation_dch_score_growth_list.append(outputs[8].correlation)
-            pvalue_dch_score_growth_list.append(outputs[8].pvalue)
-            correlation_dch_simulated_reward_list.append(outputs[9].correlation)
-            pvalue_dch_simulated_reward_list.append(outputs[9].pvalue)
-            correlation_ipw_dch_post_score_list.append(outputs[10].correlation)
-            pvalue_ipw_dch_post_score_list.append(outputs[10].pvalue)
-            correlation_ipw_dch_traj_length_list.append(outputs[11].correlation)
-            pvalue_ipw_dch_traj_length_list.append(outputs[11].pvalue)
-            correlation_ipw_dch_score_growth_list.append(outputs[12].correlation)
-            pvalue_ipw_dch_score_growth_list.append(outputs[12].pvalue)
-            correlation_ipw_dch_simulated_reward_list.append(outputs[13].correlation)
-            pvalue_ipw_dch_simulated_reward_list.append(outputs[13].pvalue)
+        for i in range(sample_size):
+            outputs = eval_policy(policy, top_k, users, items, user_records,
+                                  pretest_posttest_dict, next_item_distr_dict)
+            avg_dch_list_list.append(outputs[0])
+            avg_ipw_dch_list_list.append(outputs[1])
+            traj_posttest_score_list_list.append(outputs[2])
+            traj_score_growth_list_list.append(outputs[3])
+            correlation_dch_post_score_list.append(outputs[4].correlation)
+            pvalue_dch_post_score_list.append(outputs[4].pvalue)
+            correlation_dch_score_growth_list.append(outputs[5].correlation)
+            pvalue_dch_score_growth_list.append(outputs[5].pvalue)
+            correlation_ipw_dch_post_score_list.append(outputs[6].correlation)
+            pvalue_ipw_dch_post_score_list.append(outputs[6].pvalue)
+            correlation_ipw_dch_score_growth_list.append(outputs[7].correlation)
+            pvalue_ipw_dch_score_growth_list.append(outputs[7].pvalue)
 
         print("")
         print("{} {}".format(np.mean(correlation_dch_post_score_list),
                              np.mean(pvalue_dch_post_score_list)))
-        print("{} {}".format(np.mean(correlation_dch_traj_length_list),
-                             np.mean(pvalue_dch_traj_length_list)))
         print("{} {}".format(np.mean(correlation_dch_score_growth_list),
                              np.mean(pvalue_dch_score_growth_list)))
-        print("{} {}".format(np.mean(correlation_dch_simulated_reward_list),
-                             np.mean(pvalue_dch_simulated_reward_list)))
         print("{} {}".format(np.mean(correlation_ipw_dch_post_score_list),
                              np.mean(pvalue_ipw_dch_post_score_list)))
-        print("{} {}".format(np.mean(correlation_ipw_dch_traj_length_list),
-                             np.mean(pvalue_ipw_dch_traj_length_list)))
         print("{} {}".format(np.mean(correlation_ipw_dch_score_growth_list),
                              np.mean(pvalue_ipw_dch_score_growth_list)))
-        print("{} {}".format(np.mean(correlation_ipw_dch_simulated_reward_list),
-                             np.mean(pvalue_ipw_dch_simulated_reward_list)))
-        print("traj.-length  ", *(np.mean(traj_lengths_list, axis=0)))
         print("avg-dch  ", *np.round(np.mean(avg_dch_list_list, axis=0), 4))
         print("avg-ipw-dch  ", *np.round(np.mean(avg_ipw_dch_list_list, axis=0), 4))
         print("post-scores  ", *np.mean(traj_posttest_score_list_list, axis=0))
         print("score-growth  ", *np.round(np.mean(traj_score_growth_list_list, axis=0), 4))
-        print("rewards  ", *np.round(np.mean(final_rewards_list, axis=0), 4))
